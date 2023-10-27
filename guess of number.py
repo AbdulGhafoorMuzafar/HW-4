8cc9d8123f60f218b1af116dd1dd1d4427b7f74d
Abdul Ghafoor Muzafar <148848423+AbdulGhafoorMuzafar@users.noreply.github.com>
2023-10-27 10:58:37 -0700

    Add files via upload

---
+++ guess of number.py
@@ -0,0 +1,10 @@
+import math
+import random
+x = 0
+computer_number = random.randint(0,20)
+while True :
+    my_number = int(input("Enter your number :"))
+    x+=1
+    if (computer_number == my_number) :
+        print ("You guess correct !!! " ,x)
+        break
